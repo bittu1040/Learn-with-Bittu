7b90187964c00290278ef1ac353d06128f9620a3
Bittu <bittu1040@gmail.com>
2023-11-08 18:07:19 +0530

    sidenav mode- responsive

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {BreakpointObserver, Breakpoints} from '@angular/cdk/layout';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'first-vercal-angular-app';
+  isMobile: boolean = false;
+  constructor(private breakpointObserver: BreakpointObserver) {
+    this.isMobile = this.breakpointObserver.isMatched(Breakpoints.Handset);
+  }
 }
