1c6b5b20028a092603c6b4ae7d9c893c740841bb
Bittu <bittu1040@gmail.com>
2024-03-18 01:51:52 +0530

    added code snippet and copy option

--- src/app/components/javascript-articles/javascript-articles.component.ts
+++ src/app/components/javascript-articles/javascript-articles.component.ts
@@ -7,11 +7,33 @@ import { listOfQuestions } from 'app/shared/topics';
 })
 export class JavascriptArticlesComponent implements OnInit {
 
-JSTopics:any
-
-ngOnInit(): void {
-  this.JSTopics= listOfQuestions.filter((data)=>{
-    return data.category=="javascript"
-  })
+  JSTopics: any;
+  curryingCodeSnippet: string = `
+function SumOfThee(a){
+    return function(b){
+        return function(c){
+            return a + b + c;
+        }
+     }
 }
+
+console.log(SumOfThee(2)(3)(4));  // 9
+`;
+
+
+  ngOnInit(): void {
+    this.JSTopics = listOfQuestions.filter((data) => {
+      return data.category == "javascript"
+    })
+  }
+
+  copyToClipboard() {
+    const el = document.createElement('textarea');
+    el.value = this.curryingCodeSnippet;
+    document.body.appendChild(el);
+    el.select();
+    document.execCommand('copy');
+    document.body.removeChild(el);
+    alert('Code snippet copied to clipboard!');
+  }
 }
