816b72cb3a223e3675913c74ba6a5e779dc8e799
Bittu <bittu1040@gmail.com>
2024-03-17 10:25:44 +0530

    created skeleton for adding topics(angular and javascript)

--- src/app/components/angular-articles/angular-articles.component.ts
+++ src/app/components/angular-articles/angular-articles.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { listOfQuestions } from '../../shared/topics';
 
 @Component({
   selector: 'app-angular-articles',
   templateUrl: './angular-articles.component.html',
   styleUrls: ['./angular-articles.component.css']
 })
-export class AngularArticlesComponent {
+export class AngularArticlesComponent implements OnInit {
+
+  AngularTopics:any
+  constructor() { }
+
+  ngOnInit(): void {
+    this.AngularTopics= listOfQuestions.filter((data)=>{
+      return data.category=="angular"
+    })
+  }
 
 }
--- src/app/components/javascript-articles/javascript-articles.component.ts
+++ src/app/components/javascript-articles/javascript-articles.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { listOfQuestions } from 'app/shared/topics';
 @Component({
   selector: 'app-javascript-articles',
   templateUrl: './javascript-articles.component.html',
   styleUrls: ['./javascript-articles.component.css']
 })
-export class JavascriptArticlesComponent {
+export class JavascriptArticlesComponent implements OnInit {
 
+JSTopics:any
+
+ngOnInit(): void {
+  this.JSTopics= listOfQuestions.filter((data)=>{
+    return data.category=="javascript"
+  })
+}
 }
