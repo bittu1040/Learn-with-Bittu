9e1dbf72a761ccceb091bc0205c1e7c5cdb5b405
Bittu <bittu1040@gmail.com>
2024-04-02 17:55:51 +0530

    autocomplete implemented

--- src/app/components/snippets-image/snippets-image.component.ts
+++ src/app/components/snippets-image/snippets-image.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { DataSharingService } from 'app/shared/data-sharing.service';
+import { Observable } from 'rxjs';
+import { map, startWith } from 'rxjs/operators';
 
 @Component({
   selector: 'app-snippets-image',
@@ -9,30 +11,34 @@ import { DataSharingService } from 'app/shared/data-sharing.service';
 })
 export class SnippetsImageComponent implements OnInit {
 
-
-  exampleForm: FormGroup;
   cities: string[] = [];
+  filteredCities!: Observable<string[]>;
+  cityFormGroup!: FormGroup;
 
+  constructor(private formBuilder: FormBuilder, private data: DataSharingService) { }
 
-  constructor(private formBuilder: FormBuilder, private data: DataSharingService) {
-    this.exampleForm = this.formBuilder.group({
-      city: ['']
+  ngOnInit() {
+    this.cityFormGroup = this.formBuilder.group({
+      city: [''] // Initialize the city form control with an empty string
+    });
+
+    this.data.getCountriesList().subscribe((data: any) => {
+      data.forEach((country: any) => {
+        this.cities.push(country.name.common);
+      });
+      // Set the filteredCities observable to initially contain all cities
+      this.filteredCities = this.cityFormGroup.get('city')!.valueChanges.pipe(
+        startWith(''),
+        map(value => this._filter(value))
+      );
+    },
+    (error) => {
+      console.error('Error fetching countries:', error);
     });
   }
 
-  ngOnInit() {
-
-    this.data.getCountriesList().subscribe((data: any) => {
-      data.map((data: any) => {
-        this.cities.push(data.name.common);
-      })
-    },
-      (error) => {
-        console.error('Error fetching countries:', error);
-      }
-    )
-
+  private _filter(value: string): string[] {
+    const searchValue = value.toLowerCase();
+    return this.cities.filter(city => city.toLowerCase().includes(searchValue));
   }
-
-
 }
