9430af590be53944ae53b2eb81c8e91bea59aee7
Bittu <bittu1040@gmail.com>
2024-08-05 17:19:18 +0530

    test coverage index html

---
+++ karma.conf.js
@@ -0,0 +1,43 @@
+module.exports = function (config) {
+  config.set({
+    basePath: '',
+    frameworks: ['jasmine', '@angular-devkit/build-angular'],
+    plugins: [
+      require('karma-jasmine'),
+      require('karma-chrome-launcher'),
+      require('karma-jasmine-html-reporter'),
+      require('@angular-devkit/build-angular/plugins/karma'),
+      require('karma-coverage')
+    ],
+    client: {
+      clearContext: false // leave Jasmine Spec Runner output visible in browser
+    },
+    jasmineHtmlReporter: {
+      suppressAll: true // removes the duplicated traces
+    },
+    reporters: ['progress', 'kjhtml', 'coverage'],
+    coverageReporter: {
+      dir: require('path').join(__dirname, './coverage'),
+      subdir: '.',
+      reporters: [
+        { type: 'html' }, // Generate HTML reports
+        { type: 'text-summary' } // Generate a text summary
+      ]
+    },
+    preprocessors: {
+      'src/**/*.ts': ['coverage'], // Adjust this to match your source files
+      'src/**/*.js': ['coverage']   // Include JavaScript files if applicable
+    },    
+    coverageReporter: {
+      type: 'html',
+      dir: 'coverage/'
+    },
+    port: 9876,
+    colors: true,
+    logLevel: config.LOG_INFO,
+    autoWatch: true,
+    browsers: ['ChromeHeadless'],
+    singleRun: false,
+    restartOnFileChange: true
+  });
+};
