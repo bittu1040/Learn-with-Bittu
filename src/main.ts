d024ffb06a37323f1cfa15287d18cad3bf5e4dbe
Bittu <bittu1040@gmail.com>
2025-07-19 11:55:57 +0530

    added vercel analytics

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
+import {inject} from '@vercel/analytics'
+inject();
 
 
 platformBrowserDynamic().bootstrapModule(AppModule)
